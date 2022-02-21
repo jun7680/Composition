f6ca363be0214a48bbdbfad506b9c3a0659fa525
옥인준 <injunock@mailplug.co.kr>
2022-02-21 21:56:47 +0900

    Initial commit

---
+++ MiniSuperApp/FinanceHome/FinanceHomeRouter.swift
@@ -0,0 +1,35 @@
+import ModernRIBs
+
+protocol FinanceHomeInteractable: Interactable, SuperPayDashboardListener {
+    var router: FinanceHomeRouting? { get set }
+    var listener: FinanceHomeListener? { get set }
+}
+
+protocol FinanceHomeViewControllable: ViewControllable {
+    func addDashBoard(_ view: ViewControllable)
+}
+
+final class FinanceHomeRouter: ViewableRouter<FinanceHomeInteractable, FinanceHomeViewControllable>, FinanceHomeRouting {
+    
+    private let superPayDashboardBuildable: SuperPayDashboardBuildable
+    private var superPayRouting: Routing?
+    init(
+        interactor: FinanceHomeInteractable,
+        viewController: FinanceHomeViewControllable,
+        superPayDashboardBuildable: SuperPayDashboardBuildable
+    ) {
+        self.superPayDashboardBuildable = superPayDashboardBuildable
+        super.init(interactor: interactor, viewController: viewController)
+        interactor.router = self
+    }
+    
+    func attachSuperPayDashboard() {
+        if superPayRouting != nil { return }
+        let router = superPayDashboardBuildable.build(withListener: interactor)
+        
+        let dashboard = router.viewControllable
+        viewController.addDashBoard(dashboard)
+        
+        attachChild(router)
+    }
+}
