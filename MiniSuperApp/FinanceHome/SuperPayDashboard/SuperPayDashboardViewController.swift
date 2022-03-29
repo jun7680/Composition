adc1e3dd8b06e9ea99e16868f9d9f4e82162986d
옥인준 <injunock@mailplug.co.kr>
2022-03-29 22:29:19 +0900

    [+] viewless

--- MiniSuperApp/FinanceHome/SuperPayDashboard/SuperPayDashboardViewController.swift
+++ MiniSuperApp/FinanceHome/SuperPayDashboard/SuperPayDashboardViewController.swift
@@ -9,9 +9,7 @@ import ModernRIBs
 import UIKit
 
 protocol SuperPayDashboardPresentableListener: AnyObject {
-    // TODO: Declare properties and methods that the view controller can invoke to perform
-    // business logic, such as signIn(). This protocol is implemented by the corresponding
-    // interactor class.
+    func topupButtonDidTap()
 }
 
 final class SuperPayDashboardViewController: UIViewController, SuperPayDashboardPresentable, SuperPayDashboardViewControllable {
@@ -127,6 +125,6 @@ final class SuperPayDashboardViewController: UIViewController, SuperPayDashboard
         balanceAmountLabel.text = balance
     }
     @objc private func topupButtonDidTap() {
-        print("tap")
+        listener?.topupButtonDidTap()
     }
 }
